6e208e3e4021165fbb85281840c3edb691c4db1c
Benjamin Morris <93620006+bmorrissirromb@users.noreply.github.com>
2024-03-06 12:28:25 -0800

    better documentation

--- resolve_scp_data.py
+++ resolve_scp_data.py
@@ -21,7 +21,6 @@ import json
 import re
 
 SCP_FOLDER = "service_control_policies"
-CHILD_TYPES = ["ORGANIZATIONAL_UNIT", "ACCOUNT"]
 ACCOUNT_SUFFIX = "_ACCOUNT"  # Differentiates OU folders vs Account folders
 OUTPUT_FILE = "scp_define_attach_auto.tf"
 GLOBAL_SCP_NAME_LIST = (
@@ -32,24 +31,48 @@ logging.basicConfig(level=logging.INFO)
 
 
 def get_scp_attachments(current_target_id, current_path, data_dict, org_client):
+    """
+    Summary
+    This function will walk through the service_control_policies folder and generate SCP attachments based on its contents.
+    It will also recursively call itself to walk through the entire Organization.
+    It will also check for the presence of more than 4 attachments in an account or ROOT, and raise an exception if found.
+    It will also check for the presence of more than 2 attachments in an OU, and raise an exception if found.
+
+    Inputs
+    current_target_id - an identifier for the Organization root, OU, or account
+    current_path      - the filesystem path
+    data_dict         - The output dictionary that maps SCP names to source file paths and attachment target IDs.
+                        This is passed as an input argument so that the recursive function updates the dictionary.
+    org_client        - boto3 client for accessing Organizations API
+
+    Outputs
+    data_dict - The output dictionary that maps SCP names to source file paths and attachment target IDs
+
+    Notes
+    This function will call itself recursively in order to walk through the entire Organization
+    """
     logging.info(f"Scanning {current_path} for SCP attachments")
-    # Count the number of items in the current path and exit if more than 4
+    # Count the number of items in the current path so that we can exit if more than 4
     custom_scp_count = 0
     for each_item in os.listdir(current_path):
+        # Skip Control Tower-managed SCPs (FullAWSAccess and CT guardrails)
         if (
             os.path.isfile(os.path.join(current_path, each_item))
             and not re.search(r"\.guardrail$", each_item)
             and not re.search(r"^FullAWSAccess\.placeholder$", each_item)
         ):
             custom_scp_count += 1
+        # Check for more than 4 attachments in an OU or Account, and raise an exception if found.
         if custom_scp_count > 4:
             raise Exception(
                 f"The {current_path} folder has more than 4 attachments, making it invalid. Fix it before continuing."
             )
+        # Check for more than 2 attachments in an OU folder, and raise an exception if found.
         if not re.search(r"(ROOT|ACCOUNT)$", current_path) and custom_scp_count > 2:
             raise Exception(
                 f"The {current_path} folder is an OU folder with more than 2 custom attachments, making it invalid. Fix it before continuing."
             )
+    # Find any JSON files in the current folder and add an entry to the data dictionary representing it
     for json in glob.glob(os.path.join(current_path, "*.json"), recursive=False):
         base_name = os.path.basename(json).replace(".json", "")
         data_dict[base_name] = {
@@ -70,10 +93,11 @@ def get_scp_attachments(current_target_id, current_path, data_dict, org_client):
                 "targets": [current_target_id],
             }
     # Get all child OUs and Accounts
-    for child_type in CHILD_TYPES:
+    for child_type in ["ORGANIZATIONAL_UNIT", "ACCOUNT"]:
+        # Don't recurse into accounts
         if re.match(r"\d{12}", current_target_id):
-            # Don't recurse into accounts
             continue
+        # Get all child OUs and Accounts for the current OU
         children = org_client.list_children(
             ParentId=current_target_id, ChildType=child_type
         )
@@ -85,13 +109,15 @@ def get_scp_attachments(current_target_id, current_path, data_dict, org_client):
                     NextToken=children["NextToken"],
                 )
             )
+        # Make a recursive call for each sub-OU or Account
         for child in children["Children"]:
             object_id = child["Id"]
+            # Get the child path names from OUs/Accounts
             if child_type == "ORGANIZATIONAL_UNIT":
                 child_path_name = org_client.describe_organizational_unit(
                     OrganizationalUnitId=object_id
                 )["OrganizationalUnit"]["Name"]
-            else:
+            elif child_type == "ACCOUNT":
                 child_path_name = (
                     org_client.describe_account(AccountId=object_id)["Account"]["Name"]
                     + ACCOUNT_SUFFIX
@@ -108,7 +134,8 @@ def get_scp_attachments(current_target_id, current_path, data_dict, org_client):
                 )
             except FileNotFoundError:
                 raise FileNotFoundError(
-                    "The AWS OU structure contains a resource without a matching file/folder in the SCP repo. To resolve this, run the update_scp_ou_structure workflow."
+                    "The AWS OU structure contains a resource without a matching file/folder in the SCP repo.\n\
+                    To resolve this, run the generate_scp_ou_structure_and_imports.py script."
                 )
 
     return data_dict
@@ -123,7 +150,9 @@ def get_terraform_resource_string(
     scp_target_list,
 ):
     """
-    This function will return a string that represents a Terraform resource
+    Summary
+    This function will return a string that represents a Terraform resource.
+    This could theoretically be replaced/refactored by using jinja templating.
     """
     scp_policy_path = scp_policy_path.replace("\\", "/")
     scp_target_list_string = ", ".join(f'"{s}"' for s in scp_target_list)
