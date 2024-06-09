f71421feae9616e742bfe4985d5ca4ca70b707bf
Moumita Saha <25596163+mxs162630@users.noreply.github.com>
2024-06-09 15:42:51 -0600

    Delete static directory

--- static/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "reinforce24-iam343-tfstate-backend"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "reinforce-iam343-tfstate-lock"
-    encrypt        = true
-  }
-}
--- static/scp_module/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "scp_byte_size" {
-  value = length(var.scp_policy)
-}
